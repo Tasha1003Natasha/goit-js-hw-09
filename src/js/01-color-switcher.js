8b6d6b40fff39b466a5fd0b3731f227a1c416d08
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-22 19:32:45 +0300

    Задача № 1 готово

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+
+const bodyRef = document.querySelector('body');
+const startRef = document.querySelector(`button[data-start]`);
+const stopRef = document.querySelector(`button[data-stop]`);
+
+let timerId = null;
+
+startRef.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const colorChange = getRandomHexColor();
+    bodyRef.style.background = colorChange;
+  }, 1000);
+  if (startRef) {
+    startRef.setAttribute("disabled", "disabled");
+    stopRef.removeAttribute("disabled", "disabled");
+  }
+});
+
+stopRef.addEventListener('click', () => {
+  if (stopRef) {
+    bodyRef.style.background !== colorChange;
+    clearInterval(timerId);
+    stopRef.setAttribute("disabled", "disabled");
+    startRef.removeAttribute("disabled", "disabled");
+    
+  }
+});
