1d1ded04bcb8c0bea0d3d20772923d169455ac1f
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-23 20:06:42 +0300

    Задача № 2 (баг інпут)

---
+++ src/js/02-timer.js
@@ -0,0 +1,111 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+
+
+let selectedTime = null;
+let timerId= null;
+
+// Доступ до інпута 
+const inputRef = document.querySelector("input");
+console.log(inputRef);
+
+// Доступу до кнопки старт Start
+const btnStart = document.querySelector(`button[data-start]`);
+// console.log(btnStart);
+btnStart.setAttribute('disabled', true);
+
+// Доступ до годинника { days, hours, minutes, seconds };
+const timerRef = document.querySelector(".timer");
+// console.dir(timerRef);
+
+const daysRef = document.querySelector('[data-days]');
+// console.dir(daysRef.textContent);
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+
+// Фукція для передачі
+function getTime({ days, hours, minutes, seconds }) {
+   daysRef.textContent = days;
+   hoursRef.textContent = hours;
+   minutesRef.textContent = minutes;
+   secondsRef. textContent = seconds;
+}
+
+
+// Слухач на Start
+btnStart.addEventListener('click', hanlerClick);
+
+// Функція запуску таймера
+function hanlerClick() {
+  btnStart.setAttribute('disabled', true);
+  inputRef.setAttribute('disabled', true);
+  console.log(inputRef);
+
+  timerId = setInterval(() => {
+  let delta = selectedTime - Date.now();
+    data = convertMs(delta)
+   console.log(data);
+   getTime(data)
+      // Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00:00:00:00.
+      if (delta <= 1000) {
+        clearInterval(timerId);
+      }
+    }, 1000);
+  }
+
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedTime = selectedDates[0];
+    // Отримуємо час який є зараз
+    let now = new Date().getTime();
+    // console.log(now);
+    // дата яку вибрав користувач
+    let dateByUser = new Date(selectedDates).getTime();
+    // console.log(dateByUser);
+    
+ // / відстань між теперішнім і датою зворотного відліку
+ let ms = dateByUser - now;
+//  console.log(ms);
+    // Умова запуску таймера
+    if (dateByUser <= now) {
+      return window.alert('Please choose a date in the future');
+    } else {
+      btnStart.removeAttribute('disabled', false);
+    }
+  },
+});
+// console.log(flatpickr);
+
+function convertMs(ms) {
+
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+//Функція addLeadingZero(value), яка використовує метод padStart() 
+// і перед рендерингом інтефрейсу форматує значення.
+ function addLeadingZero(value) {
+   return String(value).padStart(2, `0`);
+}
+
